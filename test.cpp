22e2eaa3df60b8e4c036c3a0a576ef1803abcd26
akuammar1 <121154048+akuammar1@users.noreply.github.com>
2023-02-09 23:06:13 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ using namespace std;
+ int main(){
+ 
+  int a,b,add,sub,mul,div;
+  cout<<"Please Enter value 1:"<<endl;
+  cin>>a;
+  
+  cout<<"Please Enter value 2:"<<endl;
+  cin>>b;
+  
+  add=a+b;
+  cout<<"The addition is :"<<add<<endl;
+  sub=a-b;
+  cout<<"Tje subtraction is :"<<sub<<endl;
+  mul=a*b;
+  cout<<"The multiplication is :"<<mul<<endl;
+  div=a/b;
+  cout<<"The division of two number is :"<<div<<endl;
+ }
